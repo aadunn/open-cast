941bd29cfbe797a8f7914450eb2897b7ef76e01a
Dunn <a.dunn@dal.ca>
2018-01-12 17:18:17 -0400

    setup eslint rules + made some style changes

--- App.js
+++ App.js
@@ -1,58 +1,57 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Header, List, ListItem } from 'react-native-elements';
 
-export default class App extends React.Component {
-    render() {
-        const list = [
-            {
-                title: 'Web Browser',
-                icon: 'web'
-            },
-            {
-                title: 'Local Photo Library',
-                icon: 'photo-library'
-            },
-        ];
-        return (
-            <View style={ styles.main }>
-                <Header
-                    leftComponent={{
-                        icon: 'menu',
-                        color: '#fff',
-                        onPress: () => alert('menu'),
-                    }}
-                    centerComponent={{ text: 'OpenCast', style: { color: '#fff' } }}
-                    rightComponent={{
-                        icon: 'cast',
-                        color: '#fff',
-                        onPress: () => alert('casting'),
-                    }}
-                />
-                <View>
-                    <List containerStyle={{ marginTop: 100 }}>
-                        {
-                            list.map((item, i) => (
-                                <ListItem
-                                    key={ i }
-                                    title={ item.title }
-                                    leftIcon={{ name: item.icon }}
-                                    onPress={ () => {
-                                        alert('i\'ve been tapped')
-                                    }}
-                                />
-                            ))
-                        }
-                    </List>
-                </View>
-            </View>
-
-        );
-    }
-}
-
 const styles = StyleSheet.create({
-    main: {
-        backgroundColor: '#f2f2f2',
-    }
+  main: {
+    backgroundColor: '#f2f2f2',
+  },
 });
+
+export default class App extends React.Component {
+  render() {
+    const list = [
+      {
+        title: 'Web Browser',
+        icon: 'web',
+      },
+      {
+        title: 'Local Photo Library',
+        icon: 'photo-library',
+      },
+    ];
+    return (
+      <View style={styles.main}>
+        <Header
+          leftComponent={{
+            icon: 'menu',
+            color: '#fff',
+            onPress: () => alert('menu'),
+          }}
+          centerComponent={{ text: 'OpenCast', style: { color: '#fff' } }}
+          rightComponent={{
+            icon: 'cast',
+            color: '#fff',
+            onPress: () => alert('casting'),
+          }}
+        />
+        <View>
+          <List containerStyle={{ marginTop: 100 }}>
+            {
+              list.map((item, key) => (
+                <ListItem
+                  key={key}
+                  title={item.title}
+                  leftIcon={{ name: item.icon }}
+                  onPress={() => {
+                    alert('i\'ve been tapped');
+                  }}
+                />
+              ))
+            }
+          </List>
+        </View>
+      </View>
+    );
+  }
+}
