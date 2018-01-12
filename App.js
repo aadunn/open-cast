3fe40c883367b62c77c336b7903ebfed54fa0951
Dunn <a.dunn@dal.ca>
2018-01-12 16:56:59 -0400

    added new list of buttons

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { Header } from 'react-native-elements';
+import { Header, List, ListItem } from 'react-native-elements';
 
 export default class App extends React.Component {
     render() {
+        const list = [
+            {
+                title: 'Web Browser',
+                icon: 'web'
+            },
+            {
+                title: 'Local Photo Library',
+                icon: 'photo-library'
+            },
+        ];
         return (
-            <View>
+            <View style={ styles.main }>
                 <Header
-                    leftComponent={{ icon: 'menu', color: '#fff' }}
+                    leftComponent={{
+                        icon: 'menu',
+                        color: '#fff',
+                        onPress: () => alert('menu'),
+                    }}
                     centerComponent={{ text: 'OpenCast', style: { color: '#fff' } }}
-                    rightComponent={{ icon: 'cast', color: '#fff' }}
+                    rightComponent={{
+                        icon: 'cast',
+                        color: '#fff',
+                        onPress: () => alert('casting'),
+                    }}
                 />
-                <View style={{ flex: 1 }}>
-                    <Button style={{ flex: 1, border: '2px', borderColor: '#000'}}
-                        onPress={() => {
-
-                        }}
-                        title="Web Browser"
-                    />
+                <View>
+                    <List containerStyle={{ marginTop: 100 }}>
+                        {
+                            list.map((item, i) => (
+                                <ListItem
+                                    key={ i }
+                                    title={ item.title }
+                                    leftIcon={{ name: item.icon }}
+                                />
+                            ))
+                        }
+                    </List>
                 </View>
             </View>
 
@@ -26,5 +49,7 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
+    main: {
+        backgroundColor: '#f2f2f2',
+    }
 });
