ab685bffc4e233494efdf890a6c0c34af93c1374
Dunn <a.dunn@dal.ca>
2018-01-12 10:42:10 -0400

    added web browser button, still figuring out styling

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Header } from 'react-native-elements';
 
 export default class App extends React.Component {
-  render() {
-    return (
-        <Header
-            leftComponent = {{ icon: 'menu', color: '#fff' }}
-            centerComponent = {{ text: 'OpenCast', style: { color: '#fff' } }}
-            rightComponent = {{ icon: 'cast', color: '#fff' }}
-        />
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Header
+                    leftComponent={{ icon: 'menu', color: '#fff' }}
+                    centerComponent={{ text: 'OpenCast', style: { color: '#fff' } }}
+                    rightComponent={{ icon: 'cast', color: '#fff' }}
+                />
+                <View style={{ flex: 1 }}>
+                    <Button style={{ flex: 1, border: '2px', borderColor: '#000'}}
+                        onPress={() => {
+
+                        }}
+                        title="Web Browser"
+                    />
+                </View>
+            </View>
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
